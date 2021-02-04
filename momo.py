bac9358a2b4b24a0a8e6d72628572514d0b5d3d6
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2021-02-04 16:10:49 +0900

    コーディングの基本1

---
+++ momo.py
@@ -0,0 +1,27 @@
+temp = X
+X = Y
+Y = temp
+
+X, Y = Y , X
+
+if x == 'A' and y == 'B':
+    pass
+
+if (x, y) == ('A', 'B'):
+    pass
+
+idx = 0
+while idx < len(my_list):
+    print(my_list[idx])
+    idx += 1
+
+for value in my_list:
+    print(value)
+
+"""
+1行でやることは1つにする
+"""
+
+"""
+ネストは3以下が目安
+"""
