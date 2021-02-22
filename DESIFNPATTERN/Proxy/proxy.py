2f22fedc2b655dd2e1dfea5f486d68da196f87b5
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2021-02-22 16:54:13 +0900

    構造に関するパターンProxy

---
+++ DESIFNPATTERN/Proxy/proxy.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractclassmethod
+import time
+
+
+class APICaller(ABC):
+
+    @abstractclassmethod
+    def request(self):
+        pass
+
+
+class RealAPICaller(APICaller):
+
+    def __init__(self, url):
+        self.__url = url
+        time.sleep(5)
+
+    def request(self):
+        print('APIを呼び出す')
+        return
+
+
+class RealAPICallerProxy(APICaller):
+
+    def __init__(self, url):
+        self.__url = url
+
+    def __check_access(self):
+        print('アクセスに成功しました')
+        return True
+
+    def __write_log(self):
+        print('ログを出力します')
+
+    def request(self):
+        if self.__check_access():
+            real_api_caller = RealAPICaller(self.__url)
+            real_api_caller.request()
+            self.__write_log()
+
+
+#caller = RealAPICaller('http://api.com')
+#caller.request()
+
+proxy = RealAPICallerProxy('https://api.com')
+proxy.request()
