3d3badb7841f8c0205005fe33f135ce3268eaab3
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2021-03-08 09:48:39 +0900

    振る舞いに関するパターン Observer

---
+++ DESIGNPATTERN/Observer/observer.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+from random import randint
+
+
+class Subject(ABC):
+    
+    def __init__(self):
+        self._number = 0
+        self._observers = []
+
+    def attach(self, observer):
+        self._observers.append(observer)
+
+    def detach(self, observer):
+        self._observers.remove(observer)
+
+    @abstractmethod
+    def notify(self):
+        pass
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class NumberSubject(Subject):
+
+    def notify(self):
+        for observer in self._observers:
+            observer.update(self)
+
+    def change_value(self):
+        number = self._number
+        self._number = randint(0, 20)
+        print(f"number change from {number} to {self._number}")
+        self.notify()
+
+    def execute(self):
+        print('Number Subject Called')
+
+
+class Observer(ABC):
+
+    @abstractmethod
+    def update(self, subject: Subject):
+        pass
+
+
+class GraphObserver(Observer):
+
+    def update(self, subject: Subject):
+        print('GraphObserver: ' + '*' * subject._number)
+        subject.execute()
+
+
+class NumberObserver(Observer):
+
+    def update(self, subject: Subject):
+        print('NumberObserver: ' + str(subject._number))
+        subject.execute()
+
+
+subject = NumberSubject()
+
+graph_observer = GraphObserver()
+number_observer = NumberObserver()
+
+subject.attach(graph_observer)
+subject.attach(number_observer)
+subject.change_value()
